32438efdbbe8f73c7fb7a0ec3ccaa91add433950
aliismayilov <t232986@gmail.com>
2024-02-28 20:02:22 +0400

    Now code is working for 4 digit numbers but not for bigger

--- task.js
+++ task.js
@@ -1,8 +1,17 @@
 let num=prompt("Eded daxil edin")
 let sum=0
-for(let x=2;x<num.length;x++){
-    sum=Math.floor(num/10**(num.length-1))
+if(num.length==3){
+    sum=Math.floor(num/10**2)
     sum=sum+Math.floor((num/10))%10
     sum=sum+num%10
+}
+else{
+    sum=Math.floor(num/10**(num.length-1))
+    sum=sum+num%10
+    for(let x=3;num.length>x;x++){
+        sum=sum+Math.floor((num%10**(num.length-1))/10**(num.length-2))
+        sum=sum+Math.floor((num/10))%10
+    }   
+    
 }
 alert(sum)
