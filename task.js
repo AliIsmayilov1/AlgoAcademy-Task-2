ea6b45880f56c7691fdd20ff7ba6b7ddd4eccd47
aliismayilov <t232986@gmail.com>
2024-03-02 11:42:12 +0400

    Code recreated and works for all numbers

--- task.js
+++ task.js
@@ -1,17 +1,6 @@
-let num=prompt("Eded daxil edin")
 let sum=0
-if(num.length==3){
-    sum=Math.floor(num/10**2)
-    sum=sum+Math.floor((num/10))%10
-    sum=sum+num%10
-}
-else{
-    sum=Math.floor(num/10**(num.length-1))
-    sum=sum+num%10
-    for(let x=3;num.length>x;x++){
-        sum=sum+Math.floor((num%10**(num.length-1))/10**(num.length-2))
-        sum=sum+Math.floor((num/10))%10
-    }   
-    
+let num=prompt("Eded daxil edin")
+for(let x=0;x < num.length;x++){
+    sum=+num[x]+sum
 }
 alert(sum)
